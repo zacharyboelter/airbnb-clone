1fe93919ebd0bc22ea62e8b0cc6493f08bab5d26
Zachary Boelter <zboelter@gmail.com>
2022-05-06 15:45:15 -0500

    navbar and box shadow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "../images/airbnb-1.png"
+import Logo from "../images/airbnb-logo.png"
 
 
 export default function Navbar(){
-    return(<div className="nav-wrapper">
+    return(<nav className="nav-wrapper">
         <img src={Logo} alt="" className="logo-img" />
-    </div>
+    </nav>
     )
 }
