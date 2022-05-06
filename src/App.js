8144ff9909e9e070e7cd9b4694f2a4ccad58c7ef
Zachary Boelter <zboelter@gmail.com>
2022-05-06 16:48:04 -0500

    hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 export default function App() {
     return (
         <>
             <Navbar />
-           
+            <Hero />
         </>
     )
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero_img from "../images/photo-grid.png"
+
+export default function Hero() {
+    return (
+        <>
+            <div className="hero-wrapper">
+                <img src={Hero_img} alt="" className="hero-img" />
+                <div className="hero-body">
+                    <h1 className="hero-title">Online Experiences</h1>
+                    <p className="hero-copy">Join unique interactive activities led by <br /> one-of-a-kind hosts—all without leaving <br /> home.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
