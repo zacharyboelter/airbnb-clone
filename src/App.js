63f152b7fb781d1fc66619b3b9c181e862e1a307
Zachary Boelter <zboelter@gmail.com>
2022-05-07 11:56:07 -0500

    hard coded card with images and stats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Card />
         </>
     )
 }
