40e7e0795bb097773bc983670a8ae7f902bd6d69
Zachary Boelter <zboelter@gmail.com>
2022-05-07 16:17:42 -0500

    props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 
+
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
-            <Card />
+            <Card 
+                img="zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="Netherlands"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+                
+            />
         </>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,18 +5,19 @@ import star from "../images/star.png"
 
 
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props)
     return (
         <div className="card">
             <img src={photo} className="card--image" />
             <div className="card--stats">
                 <img src={star} className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray"> USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray"> {props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
