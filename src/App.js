3aaa47577e20ededfc158c71efa51ff98eee73bc
Zachary Boelter <zboelter@gmail.com>
2022-05-14 13:24:08 -0500

    map() example

--- src/App.js
+++ src/App.js
@@ -23,3 +23,20 @@ export default function App() {
         </>
     )
 }
+
+
+
+// import React from "react"
+// import Joke from "./Joke"
+// import jokesData from "./jokesData"
+
+// export default function App() {
+//     const jokeElements = jokesData.map(joke => {
+//         return <Joke setup={joke.setup} punchline={joke.punchline} />
+//     })
+//     return (
+//         <div>
+//             {jokeElements}
+//         </div>
+//     )
+// }
