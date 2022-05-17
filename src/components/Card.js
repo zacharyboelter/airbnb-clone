ebfefae186019a9777dedebcc347f3677912ca86
Zachary Boelter <zboelter@gmail.com>
2022-05-17 13:58:44 -0500

    fixed images issue!!

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`.../public/images/${props.img}`} className="card--image" />
+            <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
-                <img src=".../public/images/star.png" className="card--star" />
+                <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
